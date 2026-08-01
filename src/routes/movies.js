ce4af7c1a46962bb65270e1c14042f4893839da4
= <bartek3993@gmail.com>
2026-08-01 14:08:07 +0200

    complete crud operations for movies router in api

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -26,4 +26,73 @@ router.get("/:id", (req, res) => {
   return res.status(200).json(movie);
 });
 
+router.post("/", (req, res) => {
+  const { title, genre, year, duration, description } = req.body;
+
+  if (!title || !genre || !duration || !description) {
+    return res.status(400).json({
+      error: "INVALID_REQUEST",
+      message: "Title, genre, duration, and description are required fields",
+    });
+  }
+
+  const newMovie = {
+    id: movies.length + 1,
+    title,
+    genre,
+    duration,
+    description,
+  };
+
+  movies.push(newMovie);
+  return res.status(201).json(newMovie);
+});
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, genre, year, duration, description } = req.body;
+
+  if (!(title || genre || duration || description)) {
+    return res.status(400).json({
+      error: "INVALID_REQUEST",
+      message:
+        "Request body must contain at least one of the following fields: title, genre, duration, description",
+    });
+  }
+
+  const movieIndex = movies.findIndex((movie) => movie.id === parseInt(id));
+
+  if (movieIndex === -1) {
+    return res
+      .status(404)
+      .json({ error: "RESOURCE_NOT_FOUND", message: "Movie not found" });
+  }
+
+  const updatedMovie = {
+    ...movies[movieIndex],
+    title: title || movies[movieIndex].title,
+    genre: genre || movies[movieIndex].genre,
+    year: year || movies[movieIndex].year,
+    duration: duration || movies[movieIndex].duration,
+    description: description || movies[movieIndex].description,
+  };
+
+  movies[movieIndex] = updatedMovie;
+  return res.status(200).json(updatedMovie);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === parseInt(id, 10));
+
+  if (movieIndex === -1) {
+    return res
+      .status(404)
+      .json({ error: "RESOURCE_NOT_FOUND", message: "Movie not found" });
+  }
+
+  movies.splice(movieIndex, 1);
+  return res.status(204).send();
+});
+
 module.exports = router;
